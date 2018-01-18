409b5c66c22224cbf99723003f1b81068a6c6443
chanhi2000 <chanhi2000@gmail.com>
2018-01-18 14:29:27 +0900

    RecyclerView added for weather list

--- app/src/main/java/com/example/markiiimark/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/markiiimark/weatherapp/MainActivity.kt
@@ -2,12 +2,28 @@ package com.example.markiiimark.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val items = listOf(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thu 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7"
+
+    )
+    private val forecaseList by lazy {  findViewById(R.id.forecastList) as RecyclerView  }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        messageTextView.text = "Hello Kotlin!"
+        forecaseList.layoutManager = LinearLayoutManager(this)
+        forecaseList.adapter = ForecastListAdapter(items)
+
     }
 }
