2bfb27dc5d2b556aa33096feee0dde89447fa4f0
chanhi2000 <chanhi2000@gmail.com>
2018-01-19 15:44:15 +0900

    App Instance being Singleton applied with Delegate extension

---
+++ app/src/main/java/com/example/markiiimark/weatherapp/ui/App.kt
@@ -0,0 +1,15 @@
+package com.example.markiiimark.weatherapp.ui
+
+import android.app.Application
+import com.example.markiiimark.weatherapp.ui.utils.DelegateExt
+
+class App: Application() {
+    companion object {
+        var instance: App by DelegateExt.notNullSingleValue()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
---
+++ app/src/main/java/com/example/markiiimark/weatherapp/ui/utils/DelegateExt.kt
@@ -0,0 +1,20 @@
+package com.example.markiiimark.weatherapp.ui.utils
+
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+object DelegateExt {
+    fun <T> notNullSingleValue() = NotNullSingleValueVar<T>()
+}
+
+class NotNullSingleValueVar<T> : ReadWriteProperty<Any?, T> {
+    private var value: T? = null
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T =
+            value ?: throw IllegalStateException("${property.name} not initialized")
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        this.value = if (this.value == null) value
+        else throw IllegalStateException("${property.name} already initialized")
+    }
+}
